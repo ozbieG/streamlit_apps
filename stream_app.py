71c239c20e2cf520ea59c6eb89c345d833cc9acd
Aasban Giftson S <160816278+ozbieG@users.noreply.github.com>
2024-04-09 04:21:11 +0000

    uploader visibility fix

--- stream_app.py
+++ stream_app.py
@@ -149,22 +149,17 @@ def main():
             st.session_state.button_click1 = True
         # Train-test split
     if st.session_state.button_click1:
-        X_train, X_test, y_train, y_test = train_test_split(st.session_state.X, st.session_state.y, test_size=0.3, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(st.session_state.X, st.session_state.y, test_size=0.7, random_state=42)
 
         # Train the model and evaluate
         y_pred,y_test,accuracy = train_and_evaluate(X_train, y_train, X_test, y_test, st.session_state.selected_model)
 
         # Display average accuracy
-        st.write("Average Accuracy:", accuracy)
+        st.write("Accuracy:", accuracy)
 
         # Classification Report
         st.write("Classification Report:")
         st.write(classification_report(y_test, y_pred))
-
-        # Confusion Matrix
-        st.write("Confusion Matrix:")
-        conf_matrix = confusion_matrix(y_test, y_pred)
-        st.write(conf_matrix)
 
         # Visualize Confusion Matrix
         plt.figure(figsize=(8, 6))
