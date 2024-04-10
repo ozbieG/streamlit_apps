d7e414d3904f3219850c463485bc6e643b15360e
Aasban Giftson S <160816278+ozbieG@users.noreply.github.com>
2024-04-10 09:59:37 +0000

    caching

--- stream_app.py
+++ stream_app.py
@@ -47,10 +47,11 @@ def preprocess_data(df, feature_selection_method, feature_selection_threshold):
     yes_indices = y[y == 1].index
     no_indices = y[y == 0].index
 
-    downsampled_yes_indices = resample(yes_indices, replace=False, n_samples=int(0.1 * len(yes_indices)), random_state=42)
+    downsampled_yes_indices = resample(yes_indices, replace=False, n_samples=len(no_indices), random_state=42)
     new_indices = np.concatenate([downsampled_yes_indices, no_indices])
     X_selected = X_selected.loc[new_indices]
     y = y[new_indices]
+
     return X_selected, y, selected_features
 
 
