94bb1bcd69786ac95b877c749c27582d466e1f0e
Aasban Giftson S <160816278+ozbieG@users.noreply.github.com>
2024-04-15 06:27:22 +0000

    new

--- stream_app.py
+++ stream_app.py
@@ -86,10 +86,9 @@ def get_corr(df):
 def generate_recommended_days(probabilities, df):
     scaled_probabilities = probabilities * 100
     negative_class_probabilities = scaled_probabilities[:, 1]
-    recommended_days = np.round(np.interp(negative_class_probabilities, [15, 35, 40, 75, 95, 100], [0, 5, 10, 15, 20, 30])).astype(int)
+    recommended_days = np.round(np.interp(negative_class_probabilities, [95, 96, 97, 98, 99, 100], [0, 5, 10, 15, 20, 25, 30])).astype(int)
     recommended_days[df['machine_status'] == 0] = 0
     return recommended_days
-
 
 def main():
     st.set_option('deprecation.showPyplotGlobalUse', False)
